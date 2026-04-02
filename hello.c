4e5a04ffb144b61a3962fbb0bdd9326691935adf
SKAW-CLOUD <simokamdemabdoul@gmail.com>
2026-04-02 09:10:32 +0100

    Lab 1.1: Hello World in C and C++

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void)
+{ printf("Hello from C!\n"); printf("This program has %lu bytes in an int.\n", sizeof(int));
+return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,3 @@
+#include <iostream> using
+namespace std; int main() { cout << "Hello from C++!" << endl; cout << "This program has
+" << sizeof(int) << " bytes in an int." << endl; return 0; }
